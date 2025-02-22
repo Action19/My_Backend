ab4564c19dd71405d65bc29da636eb68752aed3f
Action19 <77909421+Action19@users.noreply.github.com>
2025-02-22 10:22:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const giftRoutes = require('./routes/giftsRoute');
 
 const allowedOrigins = [
     "https://simulate-94cp0c8wv-simulate-team.vercel.app",
-    "http://localhost:5173" // Agar lokalda test qilsangiz
+    "http://localhost:5173", // Agar lokalda test qilsangiz
+    "https://simulateuz.vercel.app"
   ];
   
 
